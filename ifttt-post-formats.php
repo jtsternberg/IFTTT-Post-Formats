815e72058f56927ae4919f785fbed57d9c511af9
Justin Sternberg <justin@dsgnwrks.pro>
2015-04-05 23:27:29 -0400

    v0.1.1, Add custom post type support

--- ifttt-post-formats.php
+++ ifttt-post-formats.php
@@ -1,54 +1,124 @@
 <?php
 /*
-	Plugin Name: IFTTT Post Formats
+	Plugin Name: IFTTT Post Formats & Post Types
 	Plugin URI: http://dsgnwrks.pro/plugins/ifttt-post-formats
-	Description: Set a post format for your IFTTT-created posts, by adding a post format category.
+	Description: Set a post format or post type for your IFTTT-created posts, by adding a post format category, or the "ifttt-posttype-{post_type_slug}" category.
 	Author URI: http://jtsternberg.com/about
 	Author: Jtsternberg
 	Donate link: http://dsgnwrks.pro/give/
-	Version: 0.1.0
+	Version: 0.1.1
 */
 
 
 class IFTTT_WP_Post_Formats {
 
+	protected $iftt_format_cats = array( 'ifttt-aside', 'ifttt-gallery', 'ifttt-link', 'ifttt-image', 'ifttt-quote', 'ifttt-status', 'ifttt-video', 'ifttt-audio', 'ifttt-chat' );
+	protected $post_id = null;
+
 	function __construct() {
-		add_action( 'save_post', array( $this, 'ifttt_formats' ), 18 );
+		add_action( 'save_post', array( $this, 'check_edit' ), 5, 2 );
+	}
+
+	/**
+	 * Check if this post is one we need to check for ifttt categories
+	 * @since 0.1.1
+	 */
+	public function check_edit( $post_id, $post ) {
+
+		if (
+			! ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			&& 'post' == $post->post_type
+			&& current_user_can( 'edit_post', $post_id )
+			&& ! wp_is_post_revision( $post )
+			&& is_null( $this->post_id )
+		) {
+			$this->post_id = $post_id;
+			add_action( 'save_post', array( $this, 'ifttt_formats_post_types' ), 18 );
+		}
+
 	}
 
 	/**
 	 * Set post format based on category, and remove category
 	 * @since 0.1.0
 	 */
-	function ifttt_formats( $post_id ) {
+	function ifttt_formats_post_types() {
 
-		$cats          = get_the_terms( $post_id, 'category' );
-		$formats       = array( 'ifttt-aside', 'ifttt-gallery', 'ifttt-link', 'ifttt-image', 'ifttt-quote', 'ifttt-status', 'ifttt-video', 'ifttt-audio', 'ifttt-chat' );
+		$cats          = get_the_terms( $this->post_id, 'category' );
 		$filtered_cats = array();
-		$share         = array();
 		$format        = false;
+		$post_type     = false;
 
-		if ( $cats ) {
-			// Loop through
-			foreach ( $cats as $key => $cat ) {
-				// if our cat slug matches one of our ifttt formats,
-				if ( in_array( $cat->slug, $formats ) ) {
-					// Set the post format
-					$format = str_replace( 'ifttt-', '', $cat->slug );
-					continue;
-				}
-				// Otherwise, add the term to the list of terms to be re-applied to the post
-				$filtered_cats[] = $cat->name;
+		// No categories? bail
+		if ( ! $cats ) {
+			return;
+		}
+
+		// Loop through
+		foreach ( $cats as $key => $cat ) {
+			// if our cat slug matches one of our ifttt formats,
+			if ( in_array( $cat->slug, $this->iftt_format_cats ) ) {
+				// Set the post format
+				$format = str_replace( 'ifttt-', '', $cat->slug );
+				continue;
 			}
+
+			if ( 0 === strpos( $cat->slug, 'ifttt-posttype-' ) ) {
+				// Set the post-type
+				$post_type = str_replace( 'ifttt-posttype-', '', $cat->slug );
+				continue;
+			}
+
+			// Otherwise, add the term to the list of terms to be re-applied to the post
+			$filtered_cats[] = $cat->name;
+		}
+
+		// If we found no post-format or post-type categories, bail
+		if ( ! $format && ! $post_type ) {
+			return;
 		}
 
 		// If we found a post-format category...
-		if ( $format && ! get_post_format( $post_id ) ) {
-			// set the post format
-			set_post_format( $post_id , $format );
+		if ( $format && ! get_post_format( $this->post_id ) ) {
+			$this->set_post_format( $format, $filtered_cats );
 		}
+
+		// If we found a post-type
+		if ( $post_type && post_type_exists( $post_type ) ) {
+			$this->set_post_type( $post_type, $filtered_cats );
+		}
+	}
+
+	/**
+	 * Set post format for a post and update the terms to remove the post-format term
+	 *
+	 * @since 0.1.1
+	 *
+	 * @param string $format        Post format to set
+	 * @param array  $filtered_cats Array of categories to re-apply to the post
+	 */
+	public function set_post_format( $format, $filtered_cats ) {
+		// set the post format
+		set_post_format( $this->post_id , $format );
+
 		// Reset terms minus ifttt post format terms
-		wp_set_object_terms( $post_id, $filtered_cats, 'category' );
+		wp_set_object_terms( $this->post_id, $filtered_cats, 'category' );
+	}
+
+	/**
+	 * Set post post_type for a post and update the terms to remove the post-type term
+	 *
+	 * @since 0.1.1
+	 *
+	 * @param string $post_type     post_type to set
+	 * @param array  $filtered_cats Array of categories to re-apply to the post
+	 */
+	public function set_post_type( $post_type, $filtered_cats ) {
+		// Reset terms minus ifttt post-type term
+		wp_set_object_terms( $this->post_id, $filtered_cats, 'category' );
+
+		// set the post-type
+		set_post_type( $this->post_id, $post_type );
 	}
 
 }
--- readme.txt
+++ readme.txt
@@ -1,29 +1,42 @@
 === Plugin Name ===
 IFTTT Post Formats
 
-Contributors: jtsternberg
-Plugin Name: IFTTT Post Formats
-Plugin URI: http://dsgnwrks.pro/plugins/ifttt-post-formats
-Tags: ifttt, post formats, if this then that, automation
-Author URI: http://jtsternberg.com/about
-Author: Jtsternberg
-Donate link: http://j.ustin.co/rYL89n
-Requires at least: 3.1
-Tested up to: 3.8
-Version: 0.1.0
-Stable tag: trunk
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Contributors: jtsternberg  
+Plugin Name: IFTTT Post Formats  
+Plugin URI: http://dsgnwrks.pro/plugins/ifttt-post-formats  
+Tags: ifttt, post formats, if this then that, automation  
+Author URI: http://jtsternberg.com/about  
+Author: Jtsternberg  
+Donate link: http://j.ustin.co/rYL89n  
+Requires at least: 3.1  
+Tested up to: 4.2.0  
+Version: 0.1.1  
+Stable tag: trunk  
+License: GPLv2 or later  
+License URI: http://www.gnu.org/licenses/gpl-2.0.html  
 
-Set a post format for your IFTTT-created posts via a post format category.
+Set a post format or post type for your IFTTT-created posts via a post format or post type category.
 
 == Description ==
 
 IFTTT (if this, then that) is one of the coolest web services available, and allows you to connect your different web service accounts to create 'recipes'. An example of a recipe that I have is to create a new WordPress post on my blog whenever I favorite a YouTube video.
 
-Unfortunately IFTTT doesn't have a way to specify a post format, so this allows you to have the post format added by using a specific category. Just add a category with 'ifttt-' + your post format, and when the post is created, it will set the post format and remove the faux category.
+Unfortunately IFTTT doesn't have a way to specify a post format or a custom post type, so this plugin provides a couple ways to update them.
 
-So for my YouTube->WordPress recipe, I have it adding the 'ifttt-video' category, and voilà, when it's published, the format has been set.
+To set the post format, you need to set the category in IFTTT to one of the following categories:
+* **`ifttt-aside`**
+* **`ifttt-gallery`**
+* **`ifttt-link`**
+* **`ifttt-image`**
+* **`ifttt-quote`**
+* **`ifttt-status`**
+* **`ifttt-video`**
+* **`ifttt-audio`**
+* **`ifttt-chat`**
+
+So for my YouTube -> WordPress recipe, I have it adding the 'ifttt-video' category, and voilà, when it's published, the format has been set.
+
+If you want to instead set the new post to a custom post type, you can do so by setting the category in IFTTT to one that matches this pattern: **`ifttt-posttype-{post_type_slug}`**. So if you wanted to create new WordPress pages with IFTTT, you would add the **`ifttt-posttype-page`** category.
 
 Hope you find this useful.
 
@@ -41,11 +54,17 @@ Hope you find this useful.
 
 == Changelog ==
 
+= 0.1.1 =
+* Add custom post type support
+
 = 0.1.0 =
 * First Release
 
 
 == Upgrade Notice ==
+
+= 0.1.1 =
+* Add custom post type support
 
 = 0.1.0 =
 * First Release
